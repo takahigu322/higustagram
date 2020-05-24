d442bf3050d51e383c00f3fec7d74e5f4c93085a
takahigu322 <takahigu642@gmail.com>
2020-05-24 12:44:34 +0900

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,20 @@
+class Post < ApplicationRecord
+
+  belongs_to :user
+
+  has_many :photos, dependent: :destroy
+
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+
+  has_many :comments, dependent: :destroy
+
+  
+  accepts_nested_attributes_for :photos
+
+  def liked_by(current_user)
+    # user_idとpost_idが一致するlikeを検索する
+    Like.find_by(user_id: current_user.id, post_id: id)
+
+  end
+
+end
